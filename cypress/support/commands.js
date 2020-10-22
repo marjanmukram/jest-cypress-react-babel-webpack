2bdb88ee94de8a3ae194d3b0fd251d1d4b4ac167
marjan <marjanmukram8@gmail.com>
2020-10-22 16:34:36 +0530

    login command added

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,11 +9,21 @@ Cypress.Commands.add('createUser', overrides => {
   }).then(response => ({...response.body.user, ...user}))
 })
 
+Cypress.Commands.add('login', user => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3000/login',
+    body: user,
+  }).then(response => {
+    window.localStorage.setItem('token', response.body.user.token)
+  })
+})
+
 Cypress.Commands.add('assertHome', () => {
   cy.url().should('eq', `${Cypress.config().baseUrl}/`)
 })
 
-Cypress.Commands.add('assertIsLoggedIn', (user) => {
+Cypress.Commands.add('assertIsLoggedIn', user => {
   cy.window()
     .its('localStorage.token')
     .should('be.a', 'string')

65c9cf9e4995acabcb0e333c29ae516fea779eea
marjan <marjanmukram8@gmail.com>
2020-10-22 14:28:58 +0530

    updated with register url

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -3,21 +3,19 @@ import {buildUser} from '../support/generate'
 describe('user should able to login', () => {
   it('should register a new user', () => {
     const user = buildUser()
-
+    cy.request({
+      url: 'http://localhost:3000/register',
+      method: 'POST',
+      body: user,
+    })
     cy.visit('/')
-    cy.findByText(/register/i).click()
-    cy.findByLabelText(/username/i).type(user.username)
-    cy.findByLabelText(/password/i).type(user.password)
-    cy.findByText(/submit/i).click()
-    cy.findByText(/logout/i).click()
+
     cy.findByText(/login/i).click()
     cy.findByLabelText(/username/i).type(user.username)
     cy.findByLabelText(/password/i).type(user.password)
-    cy.findByText(/submit/i).click()
-    .url()
-    .should('eq',`${Cypress.config().baseUrl}/`)
-
-
-     
+    cy.findByText(/submit/i)
+      .click()
+      .url()
+      .should('eq', `${Cypress.config().baseUrl}/`)
   })
 })
